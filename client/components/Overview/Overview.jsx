2ab27f6459437ed2c9a906678bc7ded122a7acba
Anisah Majeed <anisahmajeed4@gmail.com>
2022-02-16 16:36:35 -0500

    testing receive products

--- client/components/Overview/Overview.jsx
+++ client/components/Overview/Overview.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import {receiveProducts} from '../../index.js';
 
 const Overview = () => {
-  return <div> This is from Overview's Component!
-    {receiveProducts(() => {
-      console.log('it works!');
-    }) }
-
+  return <div>
+    miau
+  {receiveProducts((err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data);
+    }
+  })}
   </div>
 };
 
