5bbf2a2dcd23d7319834e24ae428e066e75b6ae1
Anisah Majeed <anisahmajeed4@gmail.com>
2022-02-22 15:59:14 -0500

    alll the data in the dropdowns whoops

--- client/components/Overview/Add-to-bag.jsx
+++ client/components/Overview/Add-to-bag.jsx
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 const AddToBag = (props) => {
 
-    console.log('STYLES', props.currentStyle.results);
+    console.log('STYLES', props.currentStyle);
 
     const resultsArray = props.currentStyle.results;
     let sizes = [];
@@ -12,7 +12,6 @@ const AddToBag = (props) => {
     for (let i = 0; i < resultsArray.length; i++) {
       // reach into results array
       let availableSizesAndQuantity = resultsArray[i].skus;
-      console.log('IS THIS AN OBJECT', availableSizesAndQuantity);
         // for each inside of here to access each individual sku
       Object.keys(availableSizesAndQuantity).forEach(style => {
         quantity[style] = availableSizesAndQuantity[style].quantity
@@ -20,17 +19,22 @@ const AddToBag = (props) => {
       });
     }
 
-    console.log('MEOW????', quantity, sizes);
+    console.log('QUANTITY THEN SIZES', quantity, sizes);
       // WE HAVE TURNED IT INTO AN ARRAYYYYYYY
         // NOW WE MUST MAP IT IN THE OPTIONS
 
     return (
     <div>
     <select>
-      <option value=''> size </option>
+    {sizes.map((size, key) =>
+      <option key={key}> {size} </option>
+    )}
+      THIS IS CURRENTLY MAPPING OVER EVERY SINGLE ONE ALL AT A TIME, CHANGE IT TO BE STYLE SPECIFIC
     </select>
     <select>
-      <option value=''> quantity </option>
+    {quantity.map((amount, key) =>
+      <option key={key}> {amount} </option>
+    )}
     </select>
     </div>
     )
