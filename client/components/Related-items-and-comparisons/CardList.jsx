d62ea2fde562170a1c57810e6b30f55c437f623c
Teresa Gobble <teresagobble@gmail.com>
2022-03-04 17:55:27 -0600

    cleaned notes and comments out of widget, app and server

--- client/components/Related-items-and-comparisons/CardList.jsx
+++ client/components/Related-items-and-comparisons/CardList.jsx
@@ -6,10 +6,6 @@ import Card from './Card.jsx';
 
 const CardList = (props) => {
 
-  // GET /products/:product_id/related <- endpoint
-  // the below is carousel scroll functionality'
-  // This is added to ensure the commit
-
   const [index, setIndex] = useState(0);
 
   const handleScrollArrow = (direction) => {
@@ -20,10 +16,9 @@ const CardList = (props) => {
     }
   }
 
-  if(props.title === "RELATED PRODUCTS") {
+  if (props.title === "RELATED PRODUCTS") {
     // this ensures that only four cards are visible on the carousel at a time
     const visibleRelatedProducts = props.relatedProducts.slice(index, index + 4);
-
 
     // the arrows only increment and decrement the index. Both are set to render under specific circumstances related to index (where the && operator wraps it and renders if the first statement is true -->[ {index > 0 && ])
     return (
@@ -31,14 +26,14 @@ const CardList = (props) => {
         <img className="embellishment" alt="embellishment" src="http://localhost:3000/assets/sectionEmbellishmentClean.png"></img>
         <h1>{props.title}</h1>
         <div>
-        {index > 0 && <img className="left-fade-out" alt="left fade out" src="http://localhost:3000/assets/fadeOut.png"/>}
-          {index > 0 && <img className="left-scroll-arrow" alt="left scroll arrow" src="http://localhost:3000/assets/leftArrow.png"  onClick={() => { handleScrollArrow('left')} } /> }
+          {index > 0 && <img className="left-fade-out" alt="left fade out" src="http://localhost:3000/assets/fadeOut.png" />}
+          {index > 0 && <img className="left-scroll-arrow" alt="left scroll arrow" src="http://localhost:3000/assets/leftArrow.png" onClick={() => { handleScrollArrow('left') }} />}
           {visibleRelatedProducts.map((relatedProductID) => (
             <Card relatedProductID={relatedProductID} key={relatedProductID} currentProduct={props.currentProduct} reviews={props.reviews} outfit={props.outfit} relatedProducts={props.relatedProducts} title={props.title} setCurrentProductID={props.setCurrentProductID} showModal={props.showModal} setCardProductFeatures={props.setCardProductFeatures} />
-            ))}
-          </div>
-          {index + 4 < props.relatedProducts.length && <img className="right-scroll-arrow" alt="right scroll arrow" src="http://localhost:3000/assets/rightArrow.png" onClick={() => { handleScrollArrow('right')} } />}
-          {index + 4 < props.relatedProducts.length && <img className="fade-out" alt="fade out" src="http://localhost:3000/assets/fadeOut.png"/>}
+          ))}
+        </div>
+        {index + 4 < props.relatedProducts.length && <img className="right-scroll-arrow" alt="right scroll arrow" src="http://localhost:3000/assets/rightArrow.png" onClick={() => { handleScrollArrow('right') }} />}
+        {index + 4 < props.relatedProducts.length && <img className="fade-out" alt="fade out" src="http://localhost:3000/assets/fadeOut.png" />}
       </div>
     )
   } else if (props.title === "YOUR OUTFIT") {
@@ -47,7 +42,7 @@ const CardList = (props) => {
         <img className="embellishment" alt="embellishment" src="http://localhost:3000/assets/sectionEmbellishmentClean.png"></img>
         <h1>{props.title}</h1>
         {props.outfit.map((outfitID) => (
-          <Card outfitID={outfitID} key={outfitID} currentProduct={props.currentProduct} reviews={props.reviews} outfit={props.outfit} setOutfit={props.setOutfit} relatedProducts={props.relatedProducts} title={props.title} setCurrentProductID={props.setCurrentProductID}/>
+          <Card outfitID={outfitID} key={outfitID} currentProduct={props.currentProduct} reviews={props.reviews} outfit={props.outfit} setOutfit={props.setOutfit} relatedProducts={props.relatedProducts} title={props.title} setCurrentProductID={props.setCurrentProductID} />
         ))}
       </div>
     )
--- client/components/Related-items-and-comparisons/Comparison.jsx
+++ client/components/Related-items-and-comparisons/Comparison.jsx
@@ -5,15 +5,13 @@ import ReactDOM from 'react-dom';
 const Comparison = (props) => {
 
   if (props.cardProductFeatures?.features === undefined || props.currentProduct?.features === undefined) {
-    //console.log("The featues needed to take a second to load here")
     return <div>Loading Card Product Features</div>
   }
 
   let tableRows = [];
-  // create an object that will populate three separate columns in the grid: a boolean value for the presence or absence of a checkmark, and the value of the feature that populates the middle column
 
   // this if conditional ensures that we iterate over the called cardProduct'd array instead of the default in App.js (which is a string)
-  if(Array.isArray(props.cardProductFeatures?.features)) {
+  if (Array.isArray(props.cardProductFeatures?.features)) {
     // this for-loop captures both shared features, and features that are only in current product (the else statement). We also have a nested conditional to render the name of the product if it's value is set to null.
     for (let i = 0; i < props.cardProductFeatures.features?.length; i++) {
       let rowObj = {};
@@ -36,14 +34,13 @@ const Comparison = (props) => {
         rowObj.doesCardProductHaveFeature = true;
         tableRows.push(rowObj);
       }
-      // console.log("tableRows[i].featureName", tableRows[0].featureName)
     }
 
     // this second for-loop captures any features that are not shared with the card product
     for (let i = 0; i < props.currentProduct.features?.length; i++) {
       let rowObj = {};
       if (!tableRows[i].featureName?.includes(props.currentProduct.features[i]?.value)) {
-        // console.log("rowObj.featureName in the 2nd for-loop", rowObj.featureName)
+
         if (props.currentProduct.features[i]?.value === null) {
           rowObj.featureName = props.currentProduct.features[i]?.feature;
         } else {
@@ -57,25 +54,23 @@ const Comparison = (props) => {
     }
   }
 
-    /*
-    Example of what data should look like in tableRows:
-      [
-        {
-          featureName: "100% Cotton",
-          doesCurrentProductHaveFeature: false,
-          doesCardProductHaveFeature: true,
-        }
-      ]
-
-
-    */
+  /*
+  Example of what data should look like in tableRows:
+    [
+      {
+        featureName: "100% Cotton",
+        doesCurrentProductHaveFeature: false,
+        doesCardProductHaveFeature: true,
+      }
+    ]
+  */
 
   return (
     <>
       {props.showModalStatus &&
-        <div className="comparison-modal-background"onClick={() => props.setShowModalStatus(prev => !prev)} >
+        <div className="comparison-modal-background" onClick={() => props.setShowModalStatus(prev => !prev)} >
           <div className="comparison-modal-wrapper">
-          <div>COMPARING</div>
+            <div>COMPARING</div>
             <div className="comparison-modal-content">
               <div className="comparision-modal-header">
                 <div>{props.currentProduct?.name}</div>
@@ -101,127 +96,4 @@ const Comparison = (props) => {
 
 }
 
-export default Comparison ;
-
-// TODO: How can I use useContext to grab the currentProduct from the App component???
-
-// GET request to https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products (make dynamic ID ':id')
-//   Current Product Name (endpoint must  match up with given ID (req.body.name):
-//   Current Product Characteristic (req.body.features) <- (this value is an array that must be iterated over with forEach):
-
-// setting current product by current product ID, with a default product upon load, "Bright Future Sunglasses"
-// const [cardProductFeatures, setCardProductFeatures] = useState(37312);
-
-// // getting current product by current product's id
-// useEffect(() => {
-//   axios.get(`/products/${cardProductFeatures}`)
-//     .then((res) => {
-//       // console.log('res: ', res); <-- working
-//       setCardProductFeatures(res.data)
-//     })
-//     .catch(err => {
-//       console.log("An error occured while fetching card item", err);
-//     })
-// }, []);
-
-
-// console.log("This is card product features", props.cardProductFeatures.features)
-  // Upon click event, the user will see this comparison card, which takes the card's product and compares it to the currentProduct.
-
-//  {props.currentProduct.features[0].feature}: {props.currentProduct.features[0].value}
-//   {props.cardProductFeatures.features[0].feature}: {props.cardProductFeatures.features[0].value}
-  // console.log("this is current product features", props.currentProduct.features)
-
-  // TODO: check for async error out and add a null placeholder for cardProductFeatures
-  // iterate over the card's product characteristics, and the currentProduct's characteristics
-  // if (cardProduct === null) {
-  //   return <div>Loading Related Products</div>
-  // }
-
-// const Comparison = (props) => {
-
-//   if (props.cardProductFeatures?.features === undefined || props.currentProduct?.features === undefined) {
-//     //console.log("The featues needed to take a second to load here")
-//     return <div>Loading Card Product Features</div>
-//   }
-
-
-//   let tableRows = [];
-//   if(Array.isArray(props.cardProductFeatures?.features)) {
-
-//     for (let i = 0; i < props.cardProductFeatures.features?.length; i++) {
-//       // for (let j = 0; j < props.cardProductFeatures.features.length; j++) {
-
-//         if (props.cardProductFeatures.features[i]?.value === props.currentProduct.features[i]?.value) {
-//           //console.log("the if conditional is working")
-//           tableRows.push(`✓   ${props.cardProductFeatures.features[i]?.value}   ✓`)
-//         } else if (props.cardProductFeatures.features[i]?.value !== props.currentProduct.features[i]?.value) {
-//           tableRows.push(`    ${props.cardProductFeatures.features[i]?.value}   ✓`)
-//           tableRows.push(`✓   ${props.currentProduct.features[i]?.value}    `)
-//         }
-//         // }
-//       }
-//       // console.log("tableRows", tableRows) <-- this works
-//     }
-
-//     // If the characteristic is shared (If the compared product value === current product value)
-//     // render the item with a checkbox in the Compared Product Name column, and in the Current Product Name column
-//     // else if the characteristic NOT shared,
-//     // render it with its product checked (and the other unchecked)
-
-//       return (
-
-//         <>
-//       {props.showModalStatus ?
-//       <div className="comparison-modal-background"onClick={() => props.setShowModalStatus(prev => !prev)} >
-//         <div className="comparison-modal-wrapper">
-//         <div>COMPARING</div>
-//           <div className="comparison-modal-content">
-//               <h4>{props.currentProduct?.name}           {props.cardProductFeatures?.name}</h4>
-//             <div className="modal-grid">
-//               <div> {tableRows.map((element, i) => { return <ul key={i} >{element}</ul>})} </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div> : null}
-//     </>
-//   )
-
-// }
-
-// export default Comparison ;
-
-// TODO: How can I use useContext to grab the currentProduct from the App component???
-
-// GET request to https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products (make dynamic ID ':id')
-//   Current Product Name (endpoint must  match up with given ID (req.body.name):
-//   Current Product Characteristic (req.body.features) <- (this value is an array that must be iterated over with forEach):
-
-// setting current product by current product ID, with a default product upon load, "Bright Future Sunglasses"
-// const [cardProductFeatures, setCardProductFeatures] = useState(37312);
-
-// // getting current product by current product's id
-// useEffect(() => {
-//   axios.get(`/products/${cardProductFeatures}`)
-//     .then((res) => {
-//       // console.log('res: ', res); <-- working
-//       setCardProductFeatures(res.data)
-//     })
-//     .catch(err => {
-//       console.log("An error occured while fetching card item", err);
-//     })
-// }, []);
-
-
-// console.log("This is card product features", props.cardProductFeatures.features)
-  // Upon click event, the user will see this comparison card, which takes the card's product and compares it to the currentProduct.
-
-//  {props.currentProduct.features[0].feature}: {props.currentProduct.features[0].value}
-//   {props.cardProductFeatures.features[0].feature}: {props.cardProductFeatures.features[0].value}
-  // console.log("this is current product features", props.currentProduct.features)
-
-  // TODO: check for async error out and add a null placeholder for cardProductFeatures
-  // iterate over the card's product characteristics, and the currentProduct's characteristics
-  // if (cardProduct === null) {
-  //   return <div>Loading Related Products</div>
-  // }
+export default Comparison;
